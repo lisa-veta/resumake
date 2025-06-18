65e43f94a94e73c951cac8e30e4d491ff98f7d1a
Elizaveta Pernikova <lisapernikowa@mail.ru>
2025-06-18 22:37:10 +0500

    feat: add icons

---
+++ src/shared/icons/index.js
@@ -0,0 +1,4 @@
+export { Envelope as EnvelopeIcon } from './Envelope';
+export { Mail as MailIcon } from './Mail';
+export { Phone as PhoneIcon } from './Phone';
+export { Telegram as TelegramIcon } from './Telegram';
